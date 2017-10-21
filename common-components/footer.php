506597c3b5174578f48d015e3c837b628fb7dc45
Imad Beyg <imad@infamous.ae>
2017-10-21 11:05:29 +0500

    faqs fix

--- common-components/footer.php
+++ common-components/footer.php
@@ -13,7 +13,8 @@
                                     <div class="panel-heading">
                                         <h4 class="panel-title">
                                             <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
-                                                What crypto-currencies are accepted in the crowd sale?</a>
+                                                What crypto-currencies are accepted in the crowd sale?
+                                            </a>
                                         </h4>
                                     </div>
                                     <div id="collapse1" class="panel-collapse collapse ">
